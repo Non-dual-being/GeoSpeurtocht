470d071f9d7f766754c8893d1bc50dc3b49840d1
Non-dual-being <139711167+Non-dual-being@users.noreply.github.com>
2026-06-24 13:04:15 +0200

    geen idee wat er is veranderd

--- src/main/java/io/github/BrianVanB/SpeurtochtModule/SpeurtochtEind.java
+++ src/main/java/io/github/BrianVanB/SpeurtochtModule/SpeurtochtEind.java
@@ -8,37 +8,33 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import io.github.BrianVanB.GeoSpeurtocht.GeoSpeurtocht;
 
-public class SpeurtochtEind extends BukkitRunnable
-{
-	private GeoSpeurtocht Master;
-	private Location dest;
-	private World world;
+public class SpeurtochtEind extends BukkitRunnable {
+    private GeoSpeurtocht Master;
+    private Location dest;
+    private World world;
 
-	public SpeurtochtEind(GeoSpeurtocht master, Location destination, World activeWorld)
-	{
-		Master = master;
-		dest = destination;
-		world = activeWorld;
-	}
+    public SpeurtochtEind(GeoSpeurtocht master, Location destination, World activeWorld) {
+        Master = master;
+        dest = destination;
+        world = activeWorld;
+    }
 
-	@Override
-	public void run()
-	{
-		GameMode targetMode = Master.getWorldConfiguredGameMode(world);
+    @Override
+    public void run() {
+        GameMode targetMode = Master.getWorldConfiguredGameMode(world);
 
-		for (Player p : Master.getServer().getOnlinePlayers())
-		{
-			if (p.isOp())
-				continue;
+        for (Player p : Master.getServer().getOnlinePlayers()) {
+            if (p.isOp())
+                continue;
 
-			if (!p.getWorld().equals(world))
-				continue;
+            if (!p.getWorld().equals(world))
+                continue;
 
-			p.teleport(dest);
-			p.setGameMode(targetMode);
-			Master.FreezeManager.Freeze(p);
-		}
+            p.teleport(dest);
+            p.setGameMode(targetMode);
+            Master.FreezeManager.Freeze(p);
+        }
 
-		this.cancel();
-	}
+        this.cancel();
+    }
 }
